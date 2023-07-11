92cd05e78cb556c7ba7fff08dabd3eac137e2325
HUYNH Henry <henry018@laposte.net>
2023-07-11 20:24:07 +0200

    anime data source interface, anime remote data source, anime api repository implementation using data source

---
+++ lib/anime/data/data_source/anime_data_source_interface.dart
@@ -0,0 +1,19 @@
+import 'package:fluttaku/anime/domain/entities/anime_episode_links_entity.dart';
+import 'package:fluttaku/anime/domain/entities/anime_info_entity.dart';
+import 'package:fluttaku/anime/domain/entities/anime_search_result_entity.dart';
+
+abstract class AnimeDataSourceInterface {
+
+  Future<AnimeSearchResultEntity> genericSearch({required String uri});
+
+  Future<AnimeSearchResultEntity> searchAnime({required String query, required int page, int pageSize = 10});
+
+  Future<AnimeSearchResultEntity> popularAnime({required int page, int pageSize = 10});
+
+  Future<AnimeSearchResultEntity> trendingAnime({required int page, int pageSize = 10});
+
+  Future<AnimeInfoEntity> fetchAnimeInfo({required String animeId});
+
+  Future<AnimeEpisodeLinksEntity> fetchEpisodeLinks({required String episodeId});
+
+}
--- lib/anime/data/repository/anime_api_repository_implementation.dart
+++ lib/anime/data/repository/anime_api_repository_implementation.dart
@@ -1,90 +1,43 @@
-import 'dart:convert';
-
-import 'package:fluttaku/anime/data/models/anime_episode_links_model.dart';
-import 'package:fluttaku/anime/data/models/anime_info_entity.dart';
-import 'package:fluttaku/anime/data/models/anime_search_result_model.dart';
+import 'package:fluttaku/anime/data/data_source/anime_data_source_interface.dart';
 import 'package:fluttaku/anime/domain/entities/anime_episode_links_entity.dart';
 import 'package:fluttaku/anime/domain/entities/anime_info_entity.dart';
+import 'package:fluttaku/anime/domain/entities/anime_search_result_entity.dart';
 import 'package:fluttaku/anime/domain/repository/anime_api_repository_interface.dart';
-import 'package:http/http.dart' as http;
 
-class AnimeAPIRepositoryImpl implements AnimeAPIRepositoryInterface {
+class AnimeApiRepositoryImpl implements AnimeAPIRepositoryInterface {
 
-  final http.Client _client = http.Client();
-  final String apiBaseUrl = 'https://api.consumet.org/meta/anilist/';
-  final String _infoEndPoint = "info/";
-  final String _watchEndPoint = "watch/";
-  final String _popularEndPoint = "popular";
-  final String _trendingEndPoint = "trending";
-  final String _pageParam = "page=";
-  final String _pageSizeParam = "perPage=";
+  final AnimeDataSourceInterface dataSource;
 
-  String pageParams({required int page, required int pageSize}) {
-    return "?$_pageParam$page&$_pageSizeParam$pageSize";
+  AnimeApiRepositoryImpl(this.dataSource);
+
+  @override
+  Future<AnimeInfoEntity> fetchAnimeInfo({required String animeId}) {
+    return dataSource.fetchAnimeInfo(animeId: animeId);
   }
 
   @override
-  Future<AnimeSearchResultModel> genericSearch({required String uri}) async {
-    final response = await _client.get(Uri.parse(uri));
-
-    if (response.statusCode != 200) {
-      //TODO: handle error
-      print("ERROR");
-    }
-
-    final Map<String, dynamic> body = await jsonDecode(response.body);
-
-    final searchResult = AnimeSearchResultModel.fromJson(body);
-    return searchResult;
+  Future<AnimeEpisodeLinksEntity> fetchEpisodeLinks({required String episodeId}) {
+    return dataSource.fetchEpisodeLinks(episodeId: episodeId);
   }
 
   @override
-  Future<AnimeSearchResultModel> searchAnime({required String query, required int page, int pageSize = 10}) async {
-    final uri = "$apiBaseUrl$query${pageParams(page: page, pageSize: pageSize)}";
-    return await genericSearch(uri: uri);
+  Future<AnimeSearchResultEntity> genericSearch({required String uri}) {
+    return dataSource.genericSearch(uri: uri);
   }
 
   @override
-  Future<AnimeSearchResultModel> popularAnime({required int page, int pageSize = 10}) async {
-    final uri = "$apiBaseUrl$_popularEndPoint${pageParams(page: page, pageSize: pageSize)}";
-    return await genericSearch(uri: uri);
+  Future<AnimeSearchResultEntity> popularAnime({required int page, int pageSize = 10}) {
+    return dataSource.popularAnime(page: page, pageSize: pageSize);
   }
 
   @override
-  Future<AnimeSearchResultModel> trendingAnime({required int page, int pageSize = 10}) async {
-    final uri = "$apiBaseUrl$_trendingEndPoint${pageParams(page: page, pageSize: pageSize)}";
-    return await genericSearch(uri: uri);
+  Future<AnimeSearchResultEntity> searchAnime({required String query, required int page, int pageSize = 10}) {
+    return dataSource.searchAnime(query: query, page: page, pageSize: pageSize);
   }
 
   @override
-  Future<AnimeInfoEntity> fetchAnimeInfo({required String animeId}) async {
-    final uri = "$apiBaseUrl$_infoEndPoint$animeId";
-    final response = await _client.get(Uri.parse(uri));
-
-    if (response.statusCode != 200) {
-      //TODO: handle error
-    }
-
-    final Map<String, dynamic> body = await jsonDecode(response.body);
-
-    final searchResult = AnimeInfoModel.fromJson(body);
-    return searchResult;
-  }
-
-  @override
-  Future<AnimeEpisodeLinksEntity> fetchEpisodeLinks({required String episodeId}) async {
-    final url = "$apiBaseUrl$_watchEndPoint$episodeId";
-    final uri = Uri.parse(url);
-
-    final response = await _client.get(uri);
-
-    if (response.statusCode != 200) {
-      //TODO: handle error
-    }
-
-    final Map<String, dynamic> body = await jsonDecode(response.body);
-    final AnimeEpisodeLinksEntity episodeLinks = AnimeEpisodeLinksModel.fromJson(body);
-    return episodeLinks;
+  Future<AnimeSearchResultEntity> trendingAnime({required int page, int pageSize = 10}) {
+    return dataSource.trendingAnime(page: page, pageSize: pageSize);
   }
 
 }
