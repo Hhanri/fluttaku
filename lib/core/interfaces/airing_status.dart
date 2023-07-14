ab24fea205283844fe7ee613d0d61f6e4e90fde9
HUYNH Henry <henry018@laposte.net>
2023-07-14 17:30:23 +0200

    airing status upcoming

--- lib/core/interfaces/airing_status.dart
+++ lib/core/interfaces/airing_status.dart
@@ -1,12 +1,14 @@
 enum AiringStatus {
   completed,
-  ongoing
+  ongoing,
+  upcoming
 }
 
 AiringStatus parseAiringStatus(String value) {
   switch(value) {
     case "Completed": return AiringStatus.completed;
     case "Ongoing": return AiringStatus.ongoing;
+    case "Not aired yet": return AiringStatus.upcoming;
     default: throw UnimplementedError("NO STATUS FOR VALUE $value");
   }
 }
