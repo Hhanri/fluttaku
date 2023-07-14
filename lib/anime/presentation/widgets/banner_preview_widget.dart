bae1c0b03b865c16bc03ba0524e9c72580d3f5b0
HUYNH Henry <henry018@laposte.net>
2023-07-14 15:09:28 +0200

    banner preview widget

---
+++ lib/anime/presentation/widgets/banner_preview_widget.dart
@@ -0,0 +1,78 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fluttaku/anime/domain/entities/anime_preview_entity.dart';
+import 'package:fluttaku/anime/presentation/widgets/cover_widget.dart';
+import 'package:fluttaku/core/config/theme.dart';
+import 'package:flutter/material.dart';
+
+class BannerPreviewWidget extends StatelessWidget {
+  final AnimePreviewEntity animePreview;
+  const BannerPreviewWidget({
+    Key? key,
+    required this.animePreview
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    const double height = 150;
+
+    return Container(
+      margin: MyTheme.defaultPadding,
+      height: height,
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: MyTheme.boxShadow,
+        borderRadius: MyTheme.defaultBorderRadius
+      ),
+      child: Stack(
+        fit: StackFit.passthrough,
+        children: [
+          ShaderMask(
+            shaderCallback: (bounds) {
+              return const LinearGradient(
+                colors: [Colors.transparent, Colors.white54, Colors.white],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0, 0.45, 1],
+              ).createShader(bounds);
+            },
+            blendMode: BlendMode.srcATop,
+            child: CachedNetworkImage(
+              imageUrl: animePreview.bannerImage,
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          Row(
+            children: [
+              CoverWidget(
+                imageUrl: animePreview.coverImage,
+                rating: animePreview.rating,
+                isAiring: animePreview.isAiring
+              ),
+              Flexible(
+                child: Padding(
+                  padding: MyTheme.defaultPadding,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        animePreview.title.romaji,
+                        overflow: TextOverflow.fade,
+                        maxLines: 2,
+                        style: MyTextStyle.titleBoldStyle,
+                      ),
+                      Text("${animePreview.totalEpisodes} episodes")
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
