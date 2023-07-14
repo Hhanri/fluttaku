905eebd05f1b33ff2af3f9adf2f6b6d9c1046e07
HUYNH Henry <henry018@laposte.net>
2023-07-14 14:48:00 +0200

    my theme

---
+++ lib/core/config/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class MyTheme {
+
+  MyTheme._();
+
+  static const boxShadow = [
+    BoxShadow(
+      color: Colors.black26,
+      spreadRadius: 0.05,
+      blurRadius: 8
+    )
+  ];
+
+  static const defaultPadding = EdgeInsets.all(12);
+
+  static final defaultBorderRadius = BorderRadius.circular(radiusValue);
+
+  static const double radiusValue = 12;
+
+}
