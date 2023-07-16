556cc2a10688920b5da626be6d43c300ce564e61
HUYNH Henry <henry018@laposte.net>
2023-07-16 18:53:43 +0200

    cover title and banner preview widgets with gesture detector

--- lib/anime/presentation/widgets/banner_preview_widget.dart
+++ lib/anime/presentation/widgets/banner_preview_widget.dart
@@ -1,9 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fluttaku/anime/domain/entities/anime_preview_entity.dart';
 import 'package:fluttaku/anime/presentation/widgets/cover_widget.dart';
+import 'package:fluttaku/core/config/router.dart';
 import 'package:fluttaku/core/config/theme.dart';
+import 'package:fluttaku/core/presentation/widgets/gradient_mask_shader.dart';
 import 'package:fluttaku/core/utils/media_query.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BannerPreviewWidget extends StatelessWidget {
   final AnimePreviewEntity animePreview;
@@ -15,63 +18,56 @@ class BannerPreviewWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-      margin: MyTheme.defaultAsymmetricPadding,
-      height: MediaQueryHelper.height(context, 0.25),
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: MyTheme.boxShadow,
-        borderRadius: MyTheme.defaultBorderRadius
-      ),
-      child: Stack(
-        fit: StackFit.passthrough,
-        children: [
-          ShaderMask(
-            shaderCallback: (bounds) {
-              return const LinearGradient(
-                colors: [Colors.transparent, Colors.white54, Colors.white],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: [0, 0.45, 1],
-              ).createShader(bounds);
-            },
-            blendMode: BlendMode.srcATop,
-            child: CachedNetworkImage(
-              imageUrl: animePreview.bannerImage ?? animePreview.coverImage,
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          Row(
-            children: [
-              CoverWidget(
-                padding: MyTheme.defaultPadding,
-                imageUrl: animePreview.coverImage,
-                rating: animePreview.rating,
-                isAiring: animePreview.isAiring
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).pushNamed(MyGoRouter.animeRouteName, extra: animePreview),
+      child: Container(
+        margin: MyTheme.defaultAsymmetricPadding,
+        height: MediaQueryHelper.height(context, 0.25),
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: MyTheme.boxShadow,
+          borderRadius: MyTheme.defaultBorderRadius
+        ),
+        child: Stack(
+          fit: StackFit.passthrough,
+          children: [
+            WhiteGradientShaderMask(
+              child: CachedNetworkImage(
+                imageUrl: animePreview.bannerImage ?? animePreview.coverImage,
+                fit: BoxFit.cover,
               ),
-              Flexible(
-                child: Padding(
+            ),
+            Row(
+              children: [
+                CoverWidget(
                   padding: MyTheme.defaultPadding,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        animePreview.title.romaji,
-                        overflow: TextOverflow.fade,
-                        maxLines: 2,
-                        style: MyTextStyle.titleBoldStyle,
-                      ),
-                      Text("${animePreview.totalEpisodes} episodes")
-                    ],
-                  ),
+                  imageUrl: animePreview.coverImage,
+                  rating: animePreview.rating,
+                  isAiring: animePreview.isAiring
                 ),
-              )
-            ],
-          ),
-        ],
+                Flexible(
+                  child: Padding(
+                    padding: MyTheme.defaultPadding,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          animePreview.title.romaji,
+                          overflow: TextOverflow.fade,
+                          maxLines: 2,
+                          style: MyTextStyle.titleBoldStyle,
+                        ),
+                        Text("${animePreview.totalEpisodes} episodes")
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/anime/presentation/widgets/cover_title_widget.dart
+++ lib/anime/presentation/widgets/cover_title_widget.dart
@@ -1,8 +1,10 @@
 import 'package:fluttaku/anime/domain/entities/anime_preview_entity.dart';
 import 'package:fluttaku/anime/presentation/widgets/cover_widget.dart';
+import 'package:fluttaku/core/config/router.dart';
 import 'package:fluttaku/core/config/theme.dart';
 import 'package:fluttaku/core/utils/media_query.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CoverTitleWidget extends StatelessWidget {
   final AnimePreviewEntity animePreview;
@@ -10,29 +12,32 @@ class CoverTitleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQueryHelper.width(context, 0.3),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Expanded(
-            flex: 4,
-            child: CoverWidget(
-              imageUrl: animePreview.coverImage,
-              rating: animePreview.rating,
-              isAiring: animePreview.isAiring
+    return GestureDetector(
+      onTap: () => GoRouter.of(context).pushNamed(MyGoRouter.animeRouteName, extra: animePreview),
+      child: SizedBox(
+        width: MediaQueryHelper.width(context, 0.3),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: CoverWidget(
+                imageUrl: animePreview.coverImage,
+                rating: animePreview.rating,
+                isAiring: animePreview.isAiring
+              ),
             ),
-          ),
-          Expanded(
-            child: Text(
-              animePreview.title.romaji,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.center,
-              style: MyTextStyle.titleStyle,
-            ),
-          )
-        ],
+            Expanded(
+              child: Text(
+                animePreview.title.romaji,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: MyTextStyle.titleStyle,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
