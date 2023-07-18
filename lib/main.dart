ebc7375fbd9d30c426acaa9a74c357d745e73075
HUYNH Henry <henry018@laposte.net>
2023-07-18 14:46:08 +0200

    intl package, debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:fluttaku/core/config/router.dart';
 import 'package:fluttaku/core/config/theme.dart';
 import 'package:fluttaku/core/service_locator.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
   setupSL();
   runApp(const MyApp());
+  initializeDateFormatting();
 }
 
 class MyApp extends StatelessWidget {
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       routerConfig: sl.get<MyGoRouter>().router,
       title: 'Fluttaku',
       theme: MyTheme.data,
